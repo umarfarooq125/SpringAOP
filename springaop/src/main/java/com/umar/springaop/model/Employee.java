52cf1441bbe90af17604f9621b3ab5b098fda851
unknown <umar.farooq@NA-PK-207.datafocal.net>
2016-04-20 11:27:09 +0500

    initial spring aop project

---
+++ springaop/src/main/java/com/umar/springaop/model/Employee.java
@@ -0,0 +1,19 @@
+package com.umar.springaop.model;
+
+public class Employee {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	//@Loggable
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void throwException() {
+		throw new RuntimeException("Dummy Exception");
+	}
+}
---
+++ springaop/src/main/java/com/umar/springaop/model/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.umar.springaop.model;
+
+public class EmployeeService {
+
+	private Employee employee;
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+}
